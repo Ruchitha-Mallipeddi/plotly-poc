e7d07d85ee1549177a0bed8e560deccb63fbf8b3
MallipeddiRuchitha <mallipeddiruchitha@gmail.com>
2021-12-06 12:27:27 +0530

    plotly.js sample project using angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LinePlotComponent } from './line-plot/line-plot.component';
+import { ExampleComponent } from './example/example.component';
+import { LineChartsComponent } from './line-charts/line-charts.component';
+import { CustomChartEventsComponent } from './custom-chart-events/custom-chart-events.component';
+
+import { TooltipComponent } from './tooltip/tooltip.component';
+import { NotesTooltipComponent } from './notes-tooltip/notes-tooltip.component';
+import { CreateNoteComponent } from './create-note/create-note.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinePlotComponent,
+    ExampleComponent,
+    LineChartsComponent,
+    CustomChartEventsComponent,
+    ChildComponent,
+    TooltipComponent,
+    NotesTooltipComponent,
+    CreateNoteComponent
   ],
   imports: [
     BrowserModule
